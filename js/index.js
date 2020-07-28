ecc19dd4c567ef91989adc732e97cc63d5f643bb
Sérgio Moura <smouracalmon@gmail.com>
2020-07-28 05:26:05 -0300

    Externalized js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
-// When the user scrolls down 50px from the top of the document, resize the header's font size
-window.addEventListener('scroll', scrollFunction);
+const TOP_OFFSET_FOR_HEADER_SHRINK = 100;
+const INITIAL_LOGO_HEIGHT = 115;
+const FINAL_LOGO_HIEGHT = 60;
 
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("logo-navbar").style.height = "60px";
-    } else {
-        document.getElementById("logo-navbar").style.height = "115px";
-    }
+function onWindowScroll() {
+    
+    let logo = document.getElementById("logo-navbar");
+    
+    if (document.body.scrollTop > TOP_OFFSET_FOR_HEADER_SHRINK ||
+        document.documentElement.scrollTop > TOP_OFFSET_FOR_HEADER_SHRINK) {
+            
+            logo.style.height = `${FINAL_LOGO_HIEGHT}px`;
+            
+        } else {
+            
+            logo.style.height = `${INITIAL_LOGO_HEIGHT}px`;
+            
+        }
+    
 }
+
+
+window.addEventListener('scroll', onWindowScroll);
